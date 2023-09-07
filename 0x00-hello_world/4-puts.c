ed0d4a020ce5ef35d41db85f00f5addde32cf505
Niyiiii <funminiyi1000@gmail.com>
2023-09-07 15:07:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with function puts
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
