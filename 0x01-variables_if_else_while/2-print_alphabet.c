72a5992e98394f7061fe7978903f1732bc407e1a
Niyiiii <funminiyi1000@gmail.com>
2023-09-08 17:17:06 -0700

    ./2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (sucessful)
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'b';
+	while
+		(a <= 'c') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
