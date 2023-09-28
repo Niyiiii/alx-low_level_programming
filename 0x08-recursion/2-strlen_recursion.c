eededc8e18c94c65a1c9672935f96715cb996963
Niyiiii <funminiyi1000@gmail.com>
2023-09-28 05:43:37 -0700

    Length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: character input
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s)
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
+	}
+	return (l);
+}
