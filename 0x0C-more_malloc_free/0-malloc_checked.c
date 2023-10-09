eda6e2d0830ef98154c6b82b26bb23af320114b8
Niyiiii <funminiyi1000@gmail.com>
2023-10-08 17:46:46 -0700

    Memory allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - function allocates memory using malloc
+ * @b: number of bytes to be allocated
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
